4317239cd43c8e140e1336a9d0e9c743378867e6
bhattrahul301 <101985869+bhattrahul301@users.noreply.github.com>
2022-04-06 20:11:14 +0530

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,4 @@
+# Write a Python program to square the elements of a list using map() function.
+numbers=input("Enter the Sample List:")
+numb=map(lambda x:x*x, numbers)
+print("The Square Root Of List Elements are:",numb)
